929807bd8756b7bdeb388ec49841671917575153
Chandra Prakassh <hevendevofficial@gmail.com>
2025-05-27 10:07:55 +0000

    updated blog admin ui here.

---
+++ app/(admin)/admin/layout.tsx
@@ -0,0 +1,25 @@
+import "@/app/globals.css";
+import { Metadata } from "next";
+import AdminHead from "@/components/AdminHeader/AdminHead";
+
+export const metadata: Metadata = {
+  title: "Badarwal - Admin",
+  description: "Badarwal Admin",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+    >
+      <body className="antialiased">
+        <AdminHead />
+        {children}
+      </body>
+    </html>
+  );
+}
